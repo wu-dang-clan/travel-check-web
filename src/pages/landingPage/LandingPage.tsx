6c9b58d6fff5d771716e1bafdb10ca38593b892d
jamooooong <andro606@naver.com>
2025-02-17 17:42:26 +0900

    🎉  start project test!

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,7 +1,14 @@
+import BgImage from "../../assets/svgs/bgImage.svg";
+import SdCara from "../../assets/svgs/sd_cara_1.svg";
 export default function LandingPage() {
   return (
     <>
-      <p className="text-3xl underline">랜딩페이지</p>
+      <div
+        className="w-lg min-w-lg mx-auto h-screen flex justify-center "
+        style={{ backgroundImage: `url(${BgImage})` }}
+      >
+        <img src={SdCara} className=" w-1/2 animate-jump-in animate-ease-out" />
+      </div>
     </>
   );
 }
